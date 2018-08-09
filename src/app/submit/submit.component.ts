301d4d1f8c89b5aee43e57a5f08d2a383a736362
lionicex <lionicex@gmail.com>
2018-08-09 19:41:42 +0100

    download and show in console.log output.json

--- src/app/submit/submit.component.ts
+++ src/app/submit/submit.component.ts
@@ -1,5 +1,6 @@
 import {Component} from '@angular/core';
 import {OutputService} from '../output.service';
+import {DomSanitizer} from '@angular/platform-browser';
 
 @Component({
     selector: 'app-submit',
@@ -8,10 +9,17 @@ import {OutputService} from '../output.service';
 })
 export class SubmitComponent {
     public selections = [];
-
-    constructor(private output: OutputService) {
+    public jsonURL;
+    private jsonString: string;
+    constructor(private output: OutputService, private sanitizer: DomSanitizer) {
         this.output.getSelections().subscribe(selections => {
             this.selections = selections;
+            this.jsonString = JSON.stringify(this.selections, null, 4);
+            this.jsonURL = this.sanitizer.bypassSecurityTrustUrl('data:text/json;charset=UTF-8,' +
+                encodeURIComponent(this.jsonString));
         });
+    }
+    outputJson() {
+        console.log(this.jsonString);
     }
 }
